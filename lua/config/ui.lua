1670a77ca252faa51e5eaa3b03f8a385e50b23c2
Luca ZH <lucarandria1@gmail.com>
2025-07-14 22:27:46 +0300

    show line numbers

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,5 +1,7 @@
 vim.cmd.colorscheme("catppuccin")
 vim.cmd("set softtabstop=3")
+vim.opt.number = true        
+vim.opt.relativenumber = true
 require("lualine").setup({ options = { theme = "catppuccin" } })
 require("neo-tree").setup({
   filesystem = {
